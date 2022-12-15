dbaef61e569bc3878ef0d4a74042737ab9750904
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-12-15 10:46:07 +0530

    dashbaord

--- pages/api/get-data.js
+++ pages/api/get-data.js
@@ -28,128 +28,127 @@ var formatDate = (dateString) => {
 let SHEET_URL = `https://api.steinhq.com/v1/storages/62e2315abca21f053ea5d9c6/Bounties%20Paid`;
 
 export async function getData(context) {
-
     let data = await Promise.all([redis.get('historic-price-data'), await axios.get(SHEET_URL)])
     let HISTORIC_DATA = JSON.parse(data[0]);
-
-
+  
+  
     let sheetData = data[1].data;
-
+  
     let todaysTotal = 0;
     sheetData.forEach((prj) => {
+      try {
+        let amount = parseInt(prj['Total Earnings USD'].replace(/,/g, ''))
+        todaysTotal = todaysTotal + amount;
+      }
+      catch (er) {
+        console.log(er)
+      }
+    })
+  
+    let positionCombinedSheet = sheetData.map((entry) => {
+      let noOfTokens = 0;
+      ['1st Prize', '2nd Prize', '3rd Prize'].forEach((pr) => {
         try {
-            let amount = parseInt(prj['Total Earnings USD'].replace(/,/g, ''))
-            todaysTotal = todaysTotal + amount;
+          if (parseInt(entry[pr].replace(/,/g, '')) > 0) {
+            noOfTokens = noOfTokens + parseInt(entry[pr].replace(/,/g, ''));
+          }
         }
         catch (er) {
-            console.log(er)
+  
         }
+      })
+      entry.totalPrize = noOfTokens;
+      return entry
     })
-
-    let positionCombinedSheet = sheetData.map((entry) => {
-        let noOfTokens = 0;
-        ['1st Prize', '2nd Prize', '3rd Prize'].forEach((pr) => {
-            try {
-                if (parseInt(entry[pr].replace(/,/g, '')) > 0) {
-                    noOfTokens = noOfTokens + parseInt(entry[pr].replace(/,/g, ''));
-                }
-            }
-            catch (er) {
-
-            }
-        })
-        entry.totalPrize = noOfTokens;
-        return entry
-    })
-
-    // let ind_date = await axios.get('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
-
+  
+    let ind_date = await axios.get('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
+  
     console.log(ind_date.data.datetime);
-
+  
     const getDateRangeArray = (range_input) => {
-        let rangeArray = [];
-        for (let i = 0; i < 10; i++) {
-            rangeArray.push(`${addSubtractDate.subtract(formatDate(new Date()), range_input * i, "days")}`)
-        }
-        return rangeArray
+      let rangeArray = [];
+      for (let i = 0; i < 10; i++) {
+        rangeArray.push(`${addSubtractDate.subtract(formatDate(ind_date.data.datetime), range_input * i, "days")}`)
+      }
+      return rangeArray
     }
-
+  
     let tokenTimePriceMap = {};
     Object.keys(HISTORIC_DATA).forEach((token) => {
-        tokenTimePriceMap[`${token}`] = {}
-        HISTORIC_DATA[token].forEach((time) => {
-            tokenTimePriceMap[`${token}`][`${time[0]}`] = time[1]
-        })
+      tokenTimePriceMap[token] = {}
+      HISTORIC_DATA[token].forEach((time) => {
+        tokenTimePriceMap[token][time[0]] = time[1]
+      })
     })
-
+  
     tokenTimePriceMap = { ...tokenTimePriceMap };
-
+  
     console.log(tokenTimePriceMap['USDT']['1633132800000']);
-
+  
     const generateGraphData = (range_input, todaysTotal) => {
-        let dateRangeArray = getDateRangeArray(range_input);
-        dateRangeArray = dateRangeArray.map((ele) => {
-            return getUnixTime(new Date(ele))
+      let dateRangeArray = getDateRangeArray(range_input);
+      dateRangeArray = dateRangeArray.map((ele) => {
+        return getUnixTime(new Date(ele))
+      })
+      let dateTokenCountArray = dateRangeArray.map((ele) => {
+        let sum = [];
+        positionCombinedSheet.forEach((entry) => {
+          let projectData = formatDate(createDate(entry['Date Given'])); // string
+          let labelDate = formatDate(unixToDate(ele)); // unix date;
+          if (projectData < labelDate) {
+            sum.push([entry['Token'], entry.totalPrize]);
+          }
         })
-        let dateTokenCountArray = dateRangeArray.map((ele) => {
-            let sum = [];
-            positionCombinedSheet.forEach((entry) => {
-                let projectData = formatDate(createDate(entry['Date Given'])); // string
-                let labelDate = formatDate(unixToDate(ele)); // unix date;
-                if (projectData < labelDate) {
-                    sum.push([entry['Token'], entry.totalPrize]);
-                }
-            })
-            return sum;
+        return sum;
+      })
+  
+      let dateTokenSumArray = dateTokenCountArray.map((dat, idx) => {
+        let sum = 0;
+        dat.forEach((pair) => {
+          if (pair[1] > 0) {
+            sum = sum + (tokenTimePriceMap[`${pair[0]}`][`${dateRangeArray[idx]}`] * pair[1])
+          }
         })
-
-        let dateTokenSumArray = dateTokenCountArray.map((dat, idx) => {
-            let sum = 0;
-            dat.forEach((pair) => {
-                if (pair[1] > 0) {
-                    sum = sum + (tokenTimePriceMap[`${pair[0]}`][`${dateRangeArray[idx]}`] * pair[1])
-                }
-            })
-            return sum
-        })
-
-
-        let xAxis = [...dateTokenSumArray];
-        console.log(xAxis)
-        xAxis[0] = todaysTotal;
-        xAxis = xAxis.map((ele) => {
-            return ele || 0
-        })
-
-        return (
-            {
-                ...{
-                    labels: dateRangeArray.map((ele) => { return (formatDate(unixToDate(ele)).toLocaleDateString()); }).reverse(),
-                    datasets: [
-                        {
-                            label: 'Dataset 1',
-                            data: xAxis.reverse(),
-                            borderColor: 'rgb(255, 255, 255)',
-                            backgroundColor: 'rgb(0, 0, 0,0.05)',
-                            tension: 0.3,
-                            fill: true,
-                        },
-                    ],
-                }
-            }
-        )
+        return sum
+      })
+  
+  
+      let xAxis = [...dateTokenSumArray];
+      console.log(xAxis)
+      xAxis[0] = todaysTotal;
+      xAxis = xAxis.map((ele) => {
+        return ele || 0
+      })
+  
+      return (
+        {
+          ...{
+            labels: dateRangeArray.map((ele) => { return (formatDate(unixToDate(ele)).toLocaleDateString()); }).reverse(),
+            datasets: [
+              {
+                label: 'Dataset 1',
+                data: xAxis.reverse(),
+                borderColor: 'rgb(255, 255, 255)',
+                backgroundColor: 'rgb(0, 0, 0,0.05)',
+                tension: 0.3,
+                fill: true,
+              },
+            ],
+          }
+        }
+      )
     }
-
+  
     const RANGE = { '3D': 3, 'W': 7, 'M': 30, '3M': 90 }
     let graphData = {
-        '3': generateGraphData(RANGE['3D'], todaysTotal),
-        '7': generateGraphData(RANGE['W'], todaysTotal),
-        '30': generateGraphData(RANGE['M'], todaysTotal),
-        '90': generateGraphData(RANGE['3M'], todaysTotal),
+      '3': generateGraphData(RANGE['3D'], todaysTotal),
+      '7': generateGraphData(RANGE['W'], todaysTotal),
+      '30': generateGraphData(RANGE['M'], todaysTotal),
+      '90': generateGraphData(RANGE['3M'], todaysTotal),
     }
-
+  
     return {
-        props: { sheetData: data[1].data, graphData: graphData, todaysTotal: todaysTotal }, // will be passed to the page component as props
+      props: { sheetData: data[1].data, graphData: graphData, todaysTotal: todaysTotal }, // will be passed to the page component as props
     }
 }
 
